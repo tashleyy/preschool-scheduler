4e7c8c2958971754a15de4d1e109b7f519aafcec
Rachel Wood <rachelvw@usc.edu>
2017-02-10 12:36:02 -0800

    Changed rate schedule model, but most things are optional. Changed rate schedules table to better reflect what stakeholder wanted

--- api/models/RateSchedule.js
+++ api/models/RateSchedule.js
@@ -3,6 +3,24 @@ module.exports = {
         name: {
             type: 'string',
             required: true
+        },
+        days: {
+            type: 'string[7]',
+            enum:['Half', 'Full', 'None'],
+            defaultsTo:'None',
+            required: false
+        },
+        cost: {
+            type: 'int',
+            required: false
+        },
+        startMonth: {
+            type: 'date',
+            required: false
+        },
+        endMonth: {
+            type: 'date',
+            required: false
         }
     }
 };
