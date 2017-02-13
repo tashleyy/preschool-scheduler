c4d04998c737f8ccc1b3fe26d8db84522a15048f
Joseph D Goelz <jgoelz@usc.edu>
2017-02-13 15:42:16 -0800

    Added a model and controller for student creation

---
+++ api/controllers/StudentController.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'create': function(req, res) {
+    var params = req.params.all();
+    if (!params.name) {  //TODO: add the rest of the params 
+      return res.badRequest();
+    }
+    RateSchedule.create(
+    {
+      name: params.name, 
+      phone: params.phone,
+      ratetSchedule: params.studentSchedule,
+      afterSchoolActivities: params.afterSchoolActivities
+    }, function studentCreated(err, student) {
+      if (err) {
+        sails.log.error(err);
+        return res.serverError();
+      }
+      return res.ok(student);
+    });
+  }
+}
---
+++ api/models/Student.js
@@ -0,0 +1,20 @@
+module.exports = {
+    attributes: {
+        name: {
+            type: 'string',
+            required: true
+        },
+        phone: {
+            type: 'string',
+            required: true
+        },
+        rateSchedule: {
+            type: 'string',
+            required: true
+        },
+        afterSchoolActivities: {
+            type: 'string',
+            required: true
+        }
+    }
+};
