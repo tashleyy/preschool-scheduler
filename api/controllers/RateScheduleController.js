87c5626da652d2dd935e661df5cf27466136bcc3
Tiffany Tjahjadi <ttjahjad@usc.edu>
2017-02-13 13:49:48 -0800

    Display rate schedules in table

--- api/controllers/RateScheduleController.js
+++ api/controllers/RateScheduleController.js
@@ -2,11 +2,12 @@ module.exports = {
   'create': function(req, res) {
     var params = req.params.all();
     if (!params.name || !params.cost || !params.monday || !params.tuesday
-    || !params.wednesday || !params.thursday || !params.friday
-    || !params.startMonth || !params.endMonth) {
+      || !params.wednesday || !params.thursday || !params.friday
+      || !params.startMonth || !params.endMonth) {
       return res.badRequest();
     }
-    RateSchedule.create({name: params.name, 
+    RateSchedule.create({
+      name: params.name, 
       cost: params.cost,
       monday: params.monday,
       tuesday: params.tuesday,
@@ -14,7 +15,8 @@ module.exports = {
       thursday: params.thursday,
       friday: params.friday,
       startMonth: params.startMonth,
-      endMonth: params.endMonth}, function rateScheduleCreated(err, rateSchedule) {
+      endMonth: params.endMonth
+    }, function rateScheduleCreated(err, rateSchedule) {
       if (err) {
         sails.log.error(err);
         return res.serverError();
--- assets/js/rateschedules.js
+++ assets/js/rateschedules.js
@@ -5,10 +5,20 @@ function displayRateSchedules() {
         success: function (data) {
             // Add table rows here
             for (var i = 0; i < data.length; i++) {
-                var rateSchedule = data[i];
-                console.log(rateSchedule);
+                var rs = data[i];
+                console.log(rs);
                 // Construct HTML string of table row
+                var rowHtml = '<tr><td>' + rs.name + '</td><td>'
+                    + rs.monday + '</td><td>'
+                    + rs.tuesday + '</td><td>'
+                    + rs.wednesday + '</td><td>'
+                    + rs.thursday + '</td><td>'
+                    + rs.friday + '</td><td>'
+                    + rs.cost + '</td><td>'
+                    + rs.startMonth + '</td><td>'
+                    + rs.endMonth + '</td></tr>';
                 // Use jQuery to add it to table body
+                $('#rate-schedule-table tbody').append(rowHtml);
             }
         },
         error: function (xhr, status, error) {
