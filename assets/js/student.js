7da37807534cd64ad7b6e90fe7a0da64c6636199
Meghan Mehta <mehtam@usc.edu>
2017-02-24 14:12:01 -0800

    student page

--- assets/js/student.js
+++ assets/js/student.js
@@ -1,27 +1,33 @@
-function fillInStudentInformation(){
+function fillInStudentInformation(name){
     $.ajax({
         url: '/student/find',
         type: 'get',
         success: function(data) {
-            var rs = data;
+
+            for (var i = 0; i < data.length; i++) {
+                if(data[i].name == name){
+                    var rs = data[i];
+                    break;
+                }
+            }
             // Construct HTML string of table row
-            var rowHtml = '<tr><td>' + "Birthday" + '<tr><td>' + "bday";
+            var rowHtml = '<tr><td>' + "Birthday" + '<tr><td>' + rs.birthday;
             $('#student-info-table tbody').append(rowHtml);
             rowHtml = '<tr><td>' + "Address" + '<tr><td>' + "address";
             $('#student-info-table tbody').append(rowHtml);
-            rowHtml = '<tr><td>' + "Parent/Guardian Names" + '<tr><td>' + "hi";
+            rowHtml = '<tr><td>' + "Parent/Guardian Names" + '<tr><td>' + rs.parent1;
             $('#student-info-table tbody').append(rowHtml);
             rowHtml = '<tr><td>' + "Parent/Guardian Phone Number" + '<tr><td>' + rs.phone;
             $('#student-info-table tbody').append(rowHtml);
-            rowHtml = '<tr><td>' + "Physician Name" + '<tr><td>' + "doctor";
+            rowHtml = '<tr><td>' + "Physician Name" + '<tr><td>' + rs.physician;
             $('#student-info-table tbody').append(rowHtml);
-            rowHtml = '<tr><td>' + "Physician Phone Number" + '<tr><td>' + "111111";
+            rowHtml = '<tr><td>' + "Physician Phone Number" + '<tr><td>' + rs.physicianPhone;
             $('#student-info-table tbody').append(rowHtml);
             rowHtml = '<tr><td>' + "Rate Schedule" + '<tr><td>' + rs.rateSchedule;
             $('#student-info-table tbody').append(rowHtml);
-            rowHtml = '<tr><td>' + "Date Enrolled" + '<tr><td>' + "today";
+            rowHtml = '<tr><td>' + "Date Enrolled" + '<tr><td>' + rs.startDate;
             $('#student-info-table tbody').append(rowHtml);
-            rowHtml = '<tr><td>' + "Date Left" + '<tr><td>' + "tomorrow";
+            rowHtml = '<tr><td>' + "Date Left" + '<tr><td>' + rs.endDate;
             $('#student-info-table tbody').append(rowHtml);
             
         },
